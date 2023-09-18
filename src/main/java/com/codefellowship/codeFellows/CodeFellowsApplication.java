8a0b99e3906fa52cbba8240671f2af229f2b7486
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-09-18 09:58:47 +0300

    Spring init

---
+++ src/main/java/com/codefellowship/codeFellows/CodeFellowsApplication.java
@@ -0,0 +1,13 @@
+package com.codefellowship.codeFellows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFellowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFellowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codefellowship/codeFellows/CodeFellowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefellowship.codeFellows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
